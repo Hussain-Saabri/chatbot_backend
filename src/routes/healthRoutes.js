b24c20e97d5ea52a331a0c9cc3d6a06850615669
Hussain-Saabri <dadapir19ce30@gmail.com>
2026-02-05 12:47:31 +0530

    error solved

--- src/routes/healthRoutes.js
+++ src/routes/healthRoutes.js
@@ -1,5 +1,5 @@
 import { healthCheck} from '../controllers/healthController.js';
 import express from 'express';
 const router = express.Router();
-router.get('/health', healthCheck);
+router.get('/', healthCheck);
 export default router;
